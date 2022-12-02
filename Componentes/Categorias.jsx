80306064f33cadc34dd7a7d5d5fe4a94a23748ed
juanmalaska <9897452@alu.murciaeduca.es>
2022-12-02 20:13:40 +0100

    commit copiar y buscar

--- Componentes/Categorias.jsx
+++ Componentes/Categorias.jsx
@@ -5,9 +5,22 @@ import Bebidas from "../assets/Bebidas.png";
 import Entrantes from "../assets/Entrantes.png";
 import Link from "next/link";
 import { UilSearchAlt } from "@iconscout/react-unicons";
+import React, { useState } from 'react';
 
 
 export default function Categorias() {
+
+  const [Order, setOrder] = useState('');
+
+  const submitValue = () => {
+    const frmdetails = {
+        'Order' : Order,
+
+    }
+    console.log(Order);
+}
+
+
   return (
     <>
  
@@ -20,40 +33,45 @@ export default function Categorias() {
                 Introduce tu numero de pedido
               </div>          
               <div className={css.divBuscar}>
-                <input className={css.input} type="text" name="buscarPedido" placeholder="ej. 123456789"/>
-                <div className={css.lupa}><UilSearchAlt/>  </div>
+                <input className={css.input} type="text" placeholder="ej. 123456789" onChange={e => setOrder(e.target.value)}/>
+                { (
+                  <Link href={`/order/${Order}`}>
+                    <button  className={css.lupa} onClick={submitValue}><UilSearchAlt/></button>
+                  </Link>
+                )}
                     
               </div>
         </div>
     </div>
-    <div className={css.heading}>
-      <span>QUE TE APETECE?</span>
-      <div className={css.services}>
-        <div className={css.feature}>
-          <div className={css.ImageWrapper}>
-            <Link href="../#entrantes">
-                <Image src={Entrantes} alt="s1" objectFit="cover" layout="intrinsic" />
-            </Link>
+      <div className={css.heading}>
+        <span>QUE TE APETECE?</span>
+        <div className={css.services}>
+          <div className={css.feature}>
+            
+            <div className={css.ImageWrapper}>
+              <Link href="../#entrantes">
+                  <Image src={Entrantes} alt="s1" objectFit="cover" layout="intrinsic" />
+              </Link>
+            </div>
+            <span className={css.categoria}>Entrantes</span>     
           </div>
-          <span className={css.categoria}>Entrantes</span>     
-        </div>
-        <div className={css.feature}>
-          <div className={css.ImageWrapper}>
-            <Link href="../#bocadillos">
-              <Image src={Bocata} alt="s1" objectFit="cover" layout="intrinsic" />
-            </Link>
+          <div className={css.feature}>
+            <div className={css.ImageWrapper}>
+              <Link href="../#bocadillos">
+                <Image src={Bocata} alt="s1" objectFit="cover" layout="intrinsic" />
+              </Link>
+            </div>
+            <span className={css.categoria}>Bocadillos</span>     
           </div>
-          <span className={css.categoria}>Bocadillos</span>     
-        </div>
-        <div className={css.feature}>
-          <div className={css.ImageWrapper}>
-            <Link href="../#bebidas">
-              <Image src={Bebidas} alt="s1" objectFit="cover" layout="intrinsic" />
-            </Link>
+          <div className={css.feature}>
+            <div className={css.ImageWrapper}>
+              <Link href="../#bebidas">
+                <Image src={Bebidas} alt="s1" objectFit="cover" layout="intrinsic" />
+              </Link>
+            </div>
+            <span className={css.categoria}>Bebidas</span>      
           </div>
-          <span className={css.categoria}>Bebidas</span>      
-        </div>
-      </div>      
+        </div>      
       </div>
     </>
   );
