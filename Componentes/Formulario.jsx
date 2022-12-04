aacb5e68af336782ed83385206e05a97582175dd
Jose <shiro19927@gmail.com>
2022-12-04 19:37:00 +0100

    Validacion Contacto terminado

--- Componentes/Formulario.jsx
+++ Componentes/Formulario.jsx
@@ -1,19 +1,18 @@
 import css from "../styles/Formulario.module.css";
-// import { useStore } from "../store/store";
 import { useState } from "react";
 import { createContacto } from "../lib/contactoHandler";
 import toast, { Toaster } from "react-hot-toast";
-import { Checkbox } from "@mantine/core";
 
 
 export default function Formulario() {
 
     const [isChecked, setIsChecked] = useState(false);
 
-    const handleOnChange = () => {
+    const handleOnChangeTrue = () => {
         setIsChecked(!isChecked);
+        console.log(setIsChecked);
       };
-      
+
 
     const [FormData, setFormData] = useState({})
 
@@ -22,9 +21,14 @@ export default function Formulario() {
     }
  
     const handleSubmit = async(e)=> {
-        e.preventDefault();
-        const id = await createContacto({...FormData});
-        toast.success("Formulario enviado.");  
+        if (!isChecked) {
+            e.preventDefault();
+            toast.success("Acepta las politicas");
+        }else{
+            e.preventDefault();
+            await createContacto({...FormData});
+            toast.success("Formulario Enviado");
+        }
     }
 
   return (
@@ -37,42 +41,44 @@ export default function Formulario() {
             <div id={css.fondo3}>
             <h2 id={css.subtitle}>Ponte en contacto con nosotros y resolveremos tus dudas en la mayor brevedad posible.</h2>
                 <form onSubmit={handleSubmit} className={css.formulario}>
-                    <span>Nombre</span>
+                    <div className={css.divSpan}>
+                        <span>Nombre</span> 
+                    </div>
                     <input type="text" name='nombre' required placeholder="Nombre" onChange={handleInput}/> 
 
-                    <span>Apellidos</span>  
+                    <div className={css.divSpan}>
+                        <span>Apellidos</span> 
+                    </div>
                     <input type="text" name='apellidos' required placeholder="Apellidos" onChange={handleInput}/>
 
-                    <span>Email</span> 
+                     <div className={css.divSpan}>
+                        <span>Email</span> 
+                    </div>
                     <input type="text" name='email' required placeholder="Email" onChange={handleInput}/>   
 
-                    <span>Asunto</span>  
+                    <div className={css.divSpan}>
+                        <span>Asunto</span> 
+                    </div> 
                     <input type="text" name='asunto' required placeholder="Asunto" onChange={handleInput}/>
 
-                    <span>Mensaje</span> 
+                    <div className={css.divSpan}>
+                        <span>Mensaje</span> 
+                    </div>
                     <textarea type="text" name='mensaje' required placeholder="Mensaje" rows={15} cols={71} onChange={handleInput}/>
 
-                    <input type="checkbox" id="topping" name="topping" value="acepto" onChange={handleOnChange}/> He leído y acepto la política de protección de datos.
-
-                    <button type='submit' className="btn">Enviar</button>
-                    <button type="reset" value="Reset Form" className="btn">Reset</button>
+                    <div className={css.divDatos}>
+                        <input type="checkbox" name="politicas" value="acepto" onChange={handleOnChangeTrue}/> 
+                        <span>He leído y acepto la política de protección de datos.</span>
+                    </div>
+                    <div className={css.divButtons}>
+                        <button type='submit' className="btn">Enviar</button>
+                        <button type="reset" value="Reset Form" className="btn">Limpiar</button>
+                    </div>
+                    
                 </form>
             </div>
         </div>
         <Toaster />
     </>
-    
-
   );
 }
-
-{/* <form action="" class={css.formulario}>
-                    <label for="fname"></label>
-                        <input type="text" id="fname" name="fname" placeholder="Tu nombre..."/>
-                    <label for="email"></label>
-                        <input type="text" id="email" name="email" placeholder="Tu email..."/>
-                    <label for="Asunto"></label>
-                        <input type="text" id="asunto" name="asunto" placeholder="Asunto..."/>
-                    <textarea name="mensaje" rows="15" cols="71" placeholder="Tu mensaje..."></textarea>
-                        <input id ={css.buttonForm}type="submit" value="Enviar"/>
-                </form>  */}
